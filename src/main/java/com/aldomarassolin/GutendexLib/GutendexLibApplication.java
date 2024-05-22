79d80f89c49472e6e4381797e6c081f14cefd049
Aldomar Assolin <assolinaldomar@gmail.com>
2024-05-22 20:44:43 -0300

    Initial project

---
+++ src/main/java/com/aldomarassolin/GutendexLib/GutendexLibApplication.java
@@ -0,0 +1,13 @@
+package com.aldomarassolin.GutendexLib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GutendexLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GutendexLibApplication.class, args);
+	}
+
+}
